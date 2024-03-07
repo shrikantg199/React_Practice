7280a41d2f4b69b58328db77d041d5073bbf6cb2
shrikantg199 <shrikantg199@gmail.com>
2024-03-07 19:36:21 +0530

    Home page

---
+++ Routing/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-9xl text-black/40 ">Home</div>
+    </div>
+  );
+};
+
+export default Home;
