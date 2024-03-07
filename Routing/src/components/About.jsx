182abcf8fef95e21ea24d4831d5276c5d1485cf1
shrikantg199 <shrikantg199@gmail.com>
2024-03-07 19:35:32 +0530

    about page

---
+++ Routing/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-9xl text-black/40 ">About</div>
+    </div>
+  );
+};
+
+export default About;
