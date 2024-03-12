83b01718c5712edb08a10e0354785d587c05dd5e
shrikantg199 <shrikantg199@gmail.com>
2024-03-12 14:56:58 +0530

    app.jsx file

---
+++ redux_practice/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "./redux/Slices";
+
+const App = () => {
+  const [input, setInput] = useState();
+  const dispatch = useDispatch();
+  const todo = useSelector((state) => state.todo.todos);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
+  return (
+    <div>
+      <form
+        action=""
+        className="justify-center flex items-center h-32 "
+        onSubmit={submitHandler}
+      >
+        {" "}
+        <input
+          className="ring-gray-600 border rounded-xl p-2 w-60"
+          placeholder="Enter todo"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      <div className=" justify-center">
+        {" "}
+        {todo.map((item, id) => (
+          <ul className="flex justify-center gap-3" key={item.id}>
+            <li>{id + 1}</li>
+            <li> {item.text}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
