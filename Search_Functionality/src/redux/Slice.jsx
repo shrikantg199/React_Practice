8104bb5712a9525ec4aefb427b6fc039d424d326
shrikantg199 <shrikantg199@gmail.com>
2024-03-15 18:49:11 +0530

    initial commit

--- Search_Functionality/src/redux/Slice.jsx
+++ Search_Functionality/src/redux/Slice.jsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const todos = createSlice({
   name: "todo",
   initialState: {
-    todos: [],
+    todos: [{ id: 1, text: hello }],
   },
   reducers: {
     addTodo: (state, action) => {
